a84be1368276f598d3fa1c633e88c3ae3c19868f
Bhumika Soam <bsoam0184@gmail.com>
2026-08-20 01:00:01 +0530

    embed.py file changed

--- backend/embed.py
+++ backend/embed.py
@@ -3,7 +3,7 @@ from sentence_transformers import SentenceTransformer
 #loading the model once at import time only
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
-def get_embedding(chunks : list[str]) -> list[float] :
+def get_embedding(chunks : list[str]) -> list[list[float]] :
     embedding = model.encode(chunks)
     return embedding.tolist()
 
@@ -20,5 +20,5 @@ if (__name__ == "__main__") :
         vec_emb = get_embedding(chunk)
         print(f"Text : {chunk[:50]}...")
         print(f"Vector length : {len(vec_emb)}")
-        print(f"First 5 values : {vec_emb[:5]}")
+        print(f"embedding  : {vec_emb}")
         print("_"*10)
